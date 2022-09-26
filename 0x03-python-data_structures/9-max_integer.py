e94f2f736315603865107ef34866a0fee6440cf5
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-09-26 23:47:31 +0300

    Task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        maxi = my_list[0]
+        for i in my_list:
+            if i > maxi:
+                maxi = i
+        return maxi
+    return None
