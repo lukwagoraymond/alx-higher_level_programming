6247d6ffeed5d72ebd54ab94c9be2e7fc1c3a753
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-09-26 22:23:58 +0300

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     """Prints string out without either 'C' or 'c'"""
     new_str = ''
-    for char in my_string:
-        if char == 'c' and char == 'C':
-            continue
-        new_str += char
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_str += i
+    return new_str
