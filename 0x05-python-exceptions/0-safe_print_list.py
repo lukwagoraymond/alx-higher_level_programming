6a85e2f0dc4112ff63dfe09f4f68ea5c58fd4358
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-10-10 20:23:31 +0300

    Task Correction1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end='')
             list_length += 1
         except IndexError:
-            pass
+            None
     print()
     return list_length
