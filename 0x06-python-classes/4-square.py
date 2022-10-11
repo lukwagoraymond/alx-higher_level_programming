fdd5be4f3b0756c123fbcb4f4b44ebab91963632
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-10-11 23:09:07 +0300

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,94 +1,48 @@
 #!/usr/bin/python3
-"""Module containing Rectangle class"""
+"""Defines a class Square"""
 
 
 class Square:
-    """Class to create a rectangle"""
-    def __init__(self, width=0, height=0):
-        """Initialize a new rectangle with `width` and `height`.
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
         Args:
-            width (int): width of rectangle with value >= 0.
-            height (int): height of rectangle with value >= 0.
+            size (int): size of a side of the square
+        Returns:
+            None
         """
-        self.width = width
-        self.height = height
-
-    @property
-    def height(self):
-        """Getter function for `height` attribute.
-
-        Returns: value of `height` attribute.
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter function for `height` attribute.
-
-        Args:
-            value (int): value to use for `height`.
-
-        Raises:
-            TypeError: If `value` is not of type int.
-            ValueError: If `value` is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value < 0:
-            raise ValueError('height must be >= 0')
-        self.__height = value
-
-    @property
-    def width(self):
-        """Getter function for `width` attribute.
-
-        Returns: value of `width` attribute.
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter function for `width` attribute.
-
-        Args:
-            value (int): value to use for `width`.
-
-        Raises:
-            TypeError: If `value` is not of type int.
-            ValueError: If `value` is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
+        self.size = size
 
     def area(self):
-        """Method to compute area of Rectangle instance"""
-        return self.width * self.height
-
-    def perimeter(self):
-        """Method to compute perimeter length of Rectangle instance.
-
-        Returns: 2 * (w + h) if both `width` and `height` > 0, else 0.
+        """calculates the square's area
+        Returns:
+            The area of the square
         """
-        return 2*(self.width + self.height) * bool(self.width and self.height)
+        return (self.__size) ** 2
 
-    def __str__(self):
-        """Format Rectangle instance for printing as a grid of `#`.
-
-        Returns: string contain `width` columns and `height` rows of `#`.
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
         """
-        rect = ""
-        for i in range(self.height):
-            rect += '#'*self.width + '\n'
-        return rect[:-1]
+        return self.__size
 
-    def __repr__(self):
-        """Format Rectangle instance so that eval(self.__repr__())
-        creates a new similar instance.
-
-        Returns: string representation to recreate object.
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
         """
-        return "Square({}, {})".format(self.width, self.height)
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
