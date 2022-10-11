4e7de4b6be1c10a69c7bed0b2f2b727fedb7b312
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-10-11 22:49:56 +0300

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,65 +1,16 @@
 #!/usr/bin/python3
-"""Module containing Square class"""
+"""Square class definition"""
 
 
 class Square:
-    """Class to create a rectangle"""
-    def __init__(self, width=0, height=0):
-        """Initialize a new rectangle with `width` and `height`.
-
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a square
         Args:
-            width (int): width of rectangle with value >= 0.
-            height (int): height of rectangle with value >= 0.
+            size (int): size of a side of the square
+        Returns: None
         """
-        self.width = width
-        self.height = height
-
-    @property
-    def height(self):
-        """Getter function for `height` attribute.
-
-        Returns: value of `height` attribute.
-        """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter function for `height` attribute.
-
-        Args:
-            value (int): value to use for `height`.
-
-        Raises:
-            TypeError: If `value` is not of type int.
-            ValueError: If `value` is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value < 0:
-            raise ValueError('height must be >= 0')
-        self.__height = value
-
-    @property
-    def width(self):
-        """Getter function for `width` attribute.
-
-        Returns: value of `width` attribute.
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Setter function for `width` attribute.
-
-        Args:
-            value (int): value to use for `width`.
-
-        Raises:
-            TypeError: If `value` is not of type int.
-            ValueError: If `value` is less than 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value < 0:
-            raise ValueError('width must be >= 0')
-        self.__width = value
+        self.__size = size
