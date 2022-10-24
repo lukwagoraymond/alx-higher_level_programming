c918313d267603dfd601c4debff2570018b0f28e
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-10-24 17:53:32 +0300

    Test Docstring Input

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,13 @@
-#!/bin/python3
+#!/usr/bin/python3
+"""Module that implements MyList Class"""
+
+
+class MyList(list):
+    """MyList class extends lists to include print_sorted method"""
+    def __init__(self):
+        """initialises the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the sorted list"""
+        return sorted(self)
