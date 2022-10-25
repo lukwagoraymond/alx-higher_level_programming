589bf594f9b2d651731dea2dd5b3fe124184315f
Raymond Lukwago <lukwagoraymond3@gmail.com>
2022-10-26 01:16:45 +0300

    Task Correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
-"""7-add_item.py
+""" 7-add_item.py
 Program to save strings from command line arguments to file called
 `add_item.json`. File contains a json serialized list of all strings
 entered as arguments to the program.
 """
 
 import sys
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    filename = "add_item.json"
 
+def add_item(args, filename):
+    """
+        adds items to a json file
+        @args (objects): The arguments that need to be added.
+        @filename: The file that needs to be updated
+    """
     try:
-        arg_list = load_from_json_file(filename)
+        content = load_from_json_file(filename)
     except IOError:
-        arg_list = []
+        content = []
 
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+    for item in args:
+        content.append(item)
+    save_to_json_file(content, filename)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    filename = "add_item.json"
+    add_item(args, filename)
