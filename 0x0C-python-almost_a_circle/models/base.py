ee6030a23f8d5c707f304ab0ec74eb7b4862c7d4
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-11-01 00:07:51 +0300

    Refactored Task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,7 +24,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """Convents python dictionaries to jason strings"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return json.dumps("[]")
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
