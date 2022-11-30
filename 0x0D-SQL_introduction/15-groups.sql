fc8898449e8997098a1a8291f3f8b5831eb07df7
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-11-30 13:57:42 +0300

    Task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- List the number of records with the same score in the table
+-- second_table under the database hbtn_0c_0 in your MySQL server
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
