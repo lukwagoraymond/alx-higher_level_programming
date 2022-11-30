076a5b764c25851d33a79070e73121577ec15783
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-11-30 10:43:26 +0300

    Task 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- Creates Table called first table in the current database
+-- first table description (columns):
+-- id INT
+-- name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `first_table`
+(id, INT,
+name VARCHAR(256));
