cb8046b0c6060184779574fe82719a53ecc24459
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-11-30 10:50:13 +0300

    Task 4 Correction

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,6 +2,4 @@
 -- first table description (columns):
 -- id INT
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS `first_table`
-(id, INT,
-name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (id INT, name VARCHAR(256));
