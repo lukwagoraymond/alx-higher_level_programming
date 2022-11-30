01cd5033c8fb5ee69fd16a4bd9f13fff2b81f83e
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-11-30 12:17:08 +0300

    Task 8 Correction

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- Displays the numer of records with id=98 in the table
 -- first_table of the database hbtn_0c_0 in your MySQL
-SELECT COUNT(*) FROM `first_name` WHERE `id` = 89;
+SELECT COUNT(*) FROM `first_name` WHERE id = 89;
