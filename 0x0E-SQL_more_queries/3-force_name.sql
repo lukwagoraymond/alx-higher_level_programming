4a394f054f509f76dfbcb9304eb0d283dffcbbcb
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-12-01 15:52:39 +0300

    Task 3

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Create a table force_name on your MySQL server
+-- force_name table description (columns):
+-- id INT
+-- name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `force_name` (id INT, name VARCHAR(256) NOT NULL);
