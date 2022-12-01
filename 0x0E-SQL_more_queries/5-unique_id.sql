46f44f8e0bf474d99bb509df49c05829711838ec
lukwagoraymond <lukwagoraymond3@gmail.com>
2022-12-01 16:01:11 +0300

    Task 5 Correction

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -2,4 +2,4 @@
 -- unique_id table description (columns):
 -- id INT DEFAULT 1
 -- name VARCHAR(256)
-CREATE TABLE IF NOT EXISTS `force_name` (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `unique_id` (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
